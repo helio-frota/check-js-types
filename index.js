99c0f6515e48b359f1b29cb45a61626b88dbb16a
Helio Frota <00hf11@gmail.com>
2020-05-08 15:38:29 -0300

    screenshots

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const { helper, noHelp } = require('./lib/helper');
 
 console.log(helper(1,2,3));
-
 console.log(helper('foo','bar',42));
 
 
 console.log(noHelp(1,2,3));
-
 console.log(noHelp('foo','bar',42));
+
