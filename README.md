070cfbcdfc1807d7fa1490bef38f5102d5611a12
Helio Frota <00hf11@gmail.com>
2020-05-08 15:31:48 -0300

    foo

---
+++ README.md
@@ -0,0 +1,10 @@
+# check-js-types
+
+Simplified example based on this video: https://www.youtube.com/watch?v=YHvqbeh_n9U
+
+Install and check the JS types with TS
+
+```
+npm i
+npm run check
+```
---
+++ index.js
@@ -0,0 +1,10 @@
+const { helper, noHelp } = require('./lib/helper');
+
+console.log(helper(1,2,3));
+
+console.log(helper('foo','bar',42));
+
+
+console.log(noHelp(1,2,3));
+
+console.log(noHelp('foo','bar',42));
